a661855deed5f8bc02f9183af995e4684fed97f2
Abhinav Kompella <kompellabhinav@gmail.com>
2024-01-19 15:28:50 -0500

    Initial Commit

---
+++ Gym Bro/Gym_BroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gym_BroApp.swift
+//  Gym Bro
+//
+//  Created by Abhinav Kompella on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Gym_BroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
